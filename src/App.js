1c4e4c35083ad05268c349e4bf65c0131c95cbd8
David Jordan <wordnerd92@gmail.com>
2023-03-26 23:05:39 -0500

    Navbar Creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { ReactDOM } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1> David </h1>
+      <Router>
+        <Navbar />
+        <Routes> 
+          <Route path="/" />
+          <Route path="cart" />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
